ef937be240a9fe836ce235dad634437c7caeab51
David Konsumer <konsumer@jetboystudio.com>
2015-11-09 19:08:17 -0800

    send version with config request

--- src/pebble-js-app.js
+++ src/pebble-js-app.js
@@ -1,7 +1,30 @@
 /* global Pebble */
 
+function getWatchVersion() {
+  // 1 = Pebble OG
+  // 2 = Pebble Steel
+  // 3 = Pebble Time
+  // 3 = Pebble Basalt Emulator (currently Pebble Time)
+  // 4 = Pebble Time Steel
+  var watch_version = 1
+  if (Pebble.getActiveWatchInfo) {
+    var watch_name = Pebble.getActiveWatchInfo().model;
+    if (watch_name.indexOf('pebble_time_steel') >= 0) {
+      watch_version = 4
+    } else if (watch_name.indexOf('pebble_time') >= 0) {
+      watch_version = 3
+    } else if (watch_name.indexOf('qemu_platform_basalt') >= 0) {
+      watch_version = 3
+    } else if (watch_name.indexOf('pebble_steel') >= 0) {
+      watch_version = 2
+    }
+  }
+  return watch_version
+}
+
 Pebble.addEventListener('showConfiguration', function () {
-  Pebble.openURL('http://konsumer.github.io/obliqe_strategies_pebble')
+  var url = 'http://konsumer.github.io/obliqe_strategies_pebble?version=' + getWatchVersion()
+  Pebble.openURL(url)
 })
 
 Pebble.addEventListener('webviewclosed', function (e) {
